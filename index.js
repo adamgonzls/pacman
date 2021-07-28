5d7af74361cf1461333c1c615b9d4f54b434e39e
Adam Gonzales <adamgonzales1@gmail.com>
2021-07-27 22:09:23 -0700

    feature: add pacDotEaten() and Ghost functionality

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const width = 28
 const grid = document.querySelector('.grid')
 const scoreDisplay = document.querySelector('#score')
 const squares = []
+let score = 0
 
 // 0 - pac-dots
 // 1 - wall
@@ -50,6 +51,8 @@ function createBoard () {
       square.classList.add('pac-dot')
     } else if (i === 1) {
       square.classList.add('wall')
+    } else if (i === 2) {
+      square.classList.add('ghost-lair')
     } else if (i === 3) {
       square.classList.add('power-pellet')
     }
@@ -67,13 +70,20 @@ function control (e) {
   switch (e.key) {
     case 'Down':
       console.log('pressed down')
-      if (!squares[pacmanCurrentIndex + width].classList.contains('wall') && pacmanCurrentIndex + width < width * width) {
+      if (
+        !squares[pacmanCurrentIndex + width].classList.contains('wall') &&
+        !squares[pacmanCurrentIndex + width].classList.contains('ghost-lair') &&
+        pacmanCurrentIndex + width < width * width) {
         pacmanCurrentIndex += width
       }
       break
     case 'ArrowDown':
       console.log('pressed down')
-      if (!squares[pacmanCurrentIndex + width].classList.contains('wall') && pacmanCurrentIndex + width < width * width) {
+      if (
+        !squares[pacmanCurrentIndex + width].classList.contains('wall') &&
+        !squares[pacmanCurrentIndex + width].classList.contains('ghost-lair') &&
+        pacmanCurrentIndex + width < width * width
+      ) {
         pacmanCurrentIndex += width
       }
       break
@@ -81,7 +91,9 @@ function control (e) {
       console.log('pressed up')
       if (
         !squares[pacmanCurrentIndex - width].classList.contains('wall') &&
-        pacmanCurrentIndex - width >= 0) {
+        !squares[pacmanCurrentIndex - width].classList.contains('ghost-lair') &&
+        pacmanCurrentIndex - width >= 0
+      ) {
         pacmanCurrentIndex -= width
       }
       break
@@ -89,7 +101,9 @@ function control (e) {
       console.log('pressed up')
       if (
         !squares[pacmanCurrentIndex - width].classList.contains('wall') &&
-        pacmanCurrentIndex - width >= 0) {
+        !squares[pacmanCurrentIndex - width].classList.contains('ghost-lair') &&
+        pacmanCurrentIndex - width >= 0
+      ) {
         pacmanCurrentIndex -= width
       }
       break
@@ -97,33 +111,111 @@ function control (e) {
       console.log('pressed left')
       if (
         !squares[pacmanCurrentIndex - 1].classList.contains('wall') &&
-        pacmanCurrentIndex % width !== 0) { pacmanCurrentIndex -= 1 }
+        !squares[pacmanCurrentIndex - 1].classList.contains('ghost-lair') &&
+        pacmanCurrentIndex % width !== 0
+      ) {
+        pacmanCurrentIndex -= 1
+        if (pacmanCurrentIndex === 364) {
+          pacmanCurrentIndex = 391
+        }
+      }
       break
     case 'ArrowLeft':
       if (
         !squares[pacmanCurrentIndex - 1].classList.contains('wall') &&
-        pacmanCurrentIndex % width !== 0) { pacmanCurrentIndex -= 1 }
+        !squares[pacmanCurrentIndex - 1].classList.contains('ghost-lair') &&
+        pacmanCurrentIndex % width !== 0
+      ) {
+        pacmanCurrentIndex -= 1
+        if (pacmanCurrentIndex === 364) {
+          pacmanCurrentIndex = 391
+        }
+      }
       break
     case 'Right':
       console.log('pressed right')
       if (
         !squares[pacmanCurrentIndex + 1].classList.contains('wall') &&
-        pacmanCurrentIndex % width < width - 1) {
+        !squares[pacmanCurrentIndex + 1].classList.contains('ghost-lair') &&
+        pacmanCurrentIndex % width < width - 1
+      ) {
         pacmanCurrentIndex += 1
+        if (pacmanCurrentIndex === 391) {
+          pacmanCurrentIndex = 364
+        }
       }
       break
     case 'ArrowRight':
       console.log('pressed right')
       if (
         !squares[pacmanCurrentIndex + 1].classList.contains('wall') &&
-        pacmanCurrentIndex % width < width - 1) {
+        !squares[pacmanCurrentIndex + 1].classList.contains('ghost-lair') &&
+        pacmanCurrentIndex % width < width - 1
+      ) {
         pacmanCurrentIndex += 1
+        if (pacmanCurrentIndex === 391) {
+          pacmanCurrentIndex = 364
+        }
       }
       break
     default:
   }
   squares[pacmanCurrentIndex].classList.add('pacman')
+  pacDotEaten()
   console.log(pacmanCurrentIndex)
 }
 
 document.addEventListener('keyup', control)
+
+function pacDotEaten () {
+  if (squares[pacmanCurrentIndex].classList.contains('pac-dot')) {
+    squares[pacmanCurrentIndex].classList.remove('pac-dot')
+    score++
+    scoreDisplay.textContent = score
+  }
+}
+
+class Ghost {
+  constructor (className, startIndex, speed) {
+    this.className = className
+    this.startIndex = startIndex
+    this.speed = speed
+    this.currentIndex = startIndex
+    this.isScared = false
+    this.timerId = NaN
+  }
+}
+
+const ghosts = [
+  new Ghost('blinky', 348, 250),
+  new Ghost('pinky', 376, 400),
+  new Ghost('inky', 351, 300),
+  new Ghost('clyde', 379, 500)
+]
+
+ghosts.forEach(ghost => {
+  squares[ghost.currentIndex].classList.add(ghost.className)
+  squares[ghost.currentIndex].classList.add('ghost')
+})
+
+ghosts.forEach(ghost => moveGhost(ghost))
+
+function moveGhost (ghost) {
+  console.log('moved ghost')
+  const directions = [-1, +1, -width, +width]
+  let direction = directions[Math.floor(Math.random() * directions.length)]
+
+  ghost.timerId = setInterval(function () {
+    if (
+      !squares[ghost.currentIndex + direction].classList.contains('wall') &&
+      !squares[ghost.currentIndex + direction].classList.contains('ghost')) {
+      squares[ghost.currentIndex].classList.remove(ghost.className)
+      squares[ghost.currentIndex].classList.remove('ghost')
+      ghost.currentIndex += direction
+      squares[ghost.currentIndex].classList.add(ghost.className)
+      squares[ghost.currentIndex].classList.add('ghost')
+    } else {
+      direction = directions[Math.floor(Math.random() * directions.length)]
+    }
+  }, ghost.speed)
+}
